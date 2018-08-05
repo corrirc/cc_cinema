ceb3c5b5f48d353de822adbae8207c52e8dbeb4f
Ricky <rickycorrigan@icloud.com>
2018-08-05 07:57:11 +0100

    Added classes to models

---
+++ models/film.rb
@@ -0,0 +1,16 @@
+require_relative("../db/sql_runner")
+
+class Film
+
+  attr_reader :id
+  attr_accessor :title, :price
+
+  def initialize
+  @id = options["id"].to_i if options["id"]
+  @title = options["title"]
+  @price = options["price"].to_i
+  end
+
+
+
+end
