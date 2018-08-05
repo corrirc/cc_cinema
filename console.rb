f4b15fb968c5be56dcb0826a1eecb0c0c7bc97f6
Ricky <rickycorrigan@icloud.com>
2018-08-05 07:19:21 +0100

    ticket data added + started db

--- console.rb
+++ console.rb
@@ -8,27 +8,42 @@ customer_1 = Customer.new({
   "funds" => "40"
 })
 
-customer_2 Customer.new({
+customer_2 = Customer.new({
   "name" => "Craig",
   "funds" => "30"
 })
 
-customer_3 Customer.new({
+customer_3 = Customer.new({
   "name" => "Lee",
   "funds" => "20"
 })
 
-film_1 Film.new({
+film_1 = Film.new({
   "title" => "Fight Club",
   "price" => "4"
 })
 
-film_2 Film.new({
+film_2 = Film.new({
   "title" => "Oceans 11",
   "price" => "5"
 })
 
-film_3 Film.new({
+film_3 = Film.new({
   "title" => "Goodfellas",
   "price" => "3"
+})
+
+ticket_1 = Ticket.new({
+  "customer_id" => customer_1.id,
+  "film_id" =>  film_1.id
+})
+
+ticket_2 = Ticket.new({
+  "customer_id" => customer_2.id,
+  "film_id" =>  film_1.id
+})
+
+ticket_3 = Ticket.new({
+  "customer_id" => customer_3.id,
+  "film_id" =>  film_2.id
 })
